6fa0322cf5fcfcb629221fa81dfe6d596b2cb722
Jack Dempsey <jdempsey1993@gmail.com>
2019-08-27 16:54:16 -0400

    Added required NPM modules to liri.js

--- liri.js
+++ liri.js
@@ -1,1 +1,26 @@
 require("dotenv").config();
+
+
+////  Adding Required modules
+
+
+
+/// Bands in Town
+
+var bandsintown = require("bandsintown")
+
+/// Module to access spotify
+
+var spotify = require("spotify")
+
+/// NPM Module for OMDB access 
+
+var request = require("request")
+
+//Module to read random.text
+
+var fs = require("fs")
+
+//Output log files 
+
+var filename = "./log.txt"
