1dad48b3e5280252c9a9b0b4c99f40a0339f017c
Jack Dempsey <jdempsey1993@gmail.com>
2019-08-28 20:53:02 -0400

    Added node spotify api

--- liri.js
+++ liri.js
@@ -1,21 +1,24 @@
 require("dotenv").config();
 
-
-////  Adding Required modules
-
-
-
 /// Bands in Town
 
 var bandsintown = require("bandsintown")
 
 /// Module to access spotify
 
-var spotify = require("spotify")
+var spotify = require("node-spotify-api")
 
 /// NPM Module for OMDB access 
 
 var request = require("request")
+
+// NPM to request Axios
+
+var axios = require("axios")
+
+//NPM Moment
+
+var moment = require("moment")
 
 //Module to read random.text
 
@@ -24,3 +27,13 @@ var fs = require("fs")
 //Output log files 
 
 var filename = "./log.txt"
+
+//Print logging information to log.text
+log.setLevel("all")
+
+
+
+//To request action
+
+var action = process.argv[2]
+
