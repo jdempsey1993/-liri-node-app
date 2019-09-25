a2e2c89aa842d6873612b716d867e1ef7cfa1459
Jack Dempsey <jdempsey1993@gmail.com>
2019-09-25 16:09:34 -0400

    Cleaned up code

--- liri.js
+++ liri.js
@@ -1,4 +1,3 @@
-  
 require("dotenv").config();
 
 var axios = require("axios");
@@ -9,44 +8,39 @@ var spotify = new Spotify(keys.spotify);
 var arg1 = process.argv[2];
 var arg2 = process.argv[3];
 
-/// Spotify Api Search
-
-function spotifyThisSong(arg) {
-    spotify.search({type: 'track', query: arg2}).then(function(response){
-        var dataOne = response.tracks.items[0]
+//Spotify Function
+function spotifyThisSong(arg2) {
+    spotify.search({ type: 'track', query: arg2 }).then(function (response) {
+        var dataOne = response.tracks.items[0];
         console.log(
             "Artist: " + dataOne.artists[0].name,
             "\nSong: " + dataOne.name,
             "\nLink to Song: " + dataOne.preview_url,
             "\nAlbum: " + dataOne.album.name
-        
-        )
-    })
-}
+        );
+    });
 
-//Axios request for OMDB
-function movieThis(arg2){
-    if (!arg2){
-        arg2 = "Mr.Nobody"
+};
+//OMDB Movie
+function movieThis(arg2) {
+    if (!arg2) {
+        arg2 = ""
     }
-    
-    axios.get("http://www.ombdapi.com/?t=" + arg2 +"&plot=short&apikey=f7e1f407").then(function(response){
+    axios.get("http://www.omdbapi.com/?t=" + arg2 + "&plot=short&apikey=trilogy").then(function (response) {
 
-        console.log(
-            "Movie Title: " + response.data.Title,
-            "\nYear: " + response.data.Year,
-            "\nIMDB Rating: " + response.data.imdbRating,
-            "\nRotten Tomatoes Rating: " + response.data.Ratings[1],
-            "\nCountry: " + response.data.Country,
-            "\nLanguage: " + response.data.Language,
-            "\nMovie Plot: " + response.data.Plot,
-            "\nActors: " + response.data.Actors)
-        })
-}
-    
+            console.log(
+                "Movie Title: " + response.data.Title,
+                "\nYear: " + response.data.Year,
+                "\nIMDB Rating: " + response.data.imdbRating,
+                "\nRotten Tomatoes Rating: " + response.data.Ratings[1],
+                "\nCountry: " + response.data.Country,
+                "\nLanguage: " + response.data.Language,
+                "\nMovie Plot: " + response.data.Plot,
+                "\nActors: " + response.data.Actors)
+    })
+};
 
-
-//Bands in town
+//Bands In Town
 function concertThis(arg2) {
     axios.get("https://rest.bandsintown.com/artists/" + arg2 + "/events?app_id=codingbootcamp").then(function (response) {
         console.log(response.data[0].venue.name)
@@ -54,38 +48,44 @@ function concertThis(arg2) {
         console.log(moment(response.data[0].datetime).format("MM-DD-YYYY"))
     }
     )
+}
 
-// Function to log text
 
-function logging (){
+// logging output in random.txt 
 
-    var fs= require("fs")
+function doWhatItSays() {
 
-    fs.readFile("random.txt","utf8",function(error,data){
+    var fs = require("fs")
+
+    
+    
+    fs.readFile("random.txt", "utf8", function (error, data) {
         if (error) {
             return console.log(error)
-        }
-
-        var dataArray = data.split(",")
+        } 
+        
+        var dataArray = data.split(',')
         arg1 = dataArray[0]
         arg2 = dataArray[1]
-        console.log(spotifyThisSong(arg2))
-    })
+        console.log(spotifyThisSong(arg2));
+    });
+    
+};
 
- } switch (arg1) {
-     case "movie-this":
-         movieThis(arg2)
-         break
+switch (arg1) {
+    case "movie-this":
+        movieThis(arg2)
+        break;
 
-     case "concert-this":
+    case "concert-this":
         concertThis(arg2)
-        break
+        break;
 
     case "spotify-this-song":
         spotifyThisSong(arg2)
+        break;
 
-    case "logging":
-        logging()
-        break
- }
-}
+    case "do-what-it-says":
+        doWhatItSays()
+        break;
+};
